833a5c4ed61783e109899d53cd04ef4a9a7f6847
BasttlBusiness <121219206+BasttlBusiness@users.noreply.github.com>
2022-12-24 17:03:08 +0530

    Update index.php

--- index.php
+++ index.php
@@ -24,7 +24,7 @@ get_header();
 				?>
 				<header>
 					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-				</header>
+			</header>
 				<?php
 				$breadcrumb_enable = get_theme_mod( 'collective_news_breadcrumb_enable', true );
 				if ( $breadcrumb_enable ) :
